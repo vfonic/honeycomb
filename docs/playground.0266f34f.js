5b4ef64bdd7f71a94bbd1d01cfaff4f35799cd48
Viktor <viktor.fonic@gmail.com>
2021-03-28 19:50:58 +0200

    Fix build

--- docs/playground.0266f34f.js
+++ docs/playground.0266f34f.js
@@ -2278,7 +2278,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49514" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52017" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -2455,4 +2455,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","playground/index.ts"], null)
-//# sourceMappingURL=/playground.0266f34f.js.map
+//# sourceMappingURL=honeycomb/playground.0266f34f.js.map
