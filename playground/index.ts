6f4f71ac07daf3519a8cb8d705f9698134abc406
Viktor <viktor.fonic@gmail.com>
2021-03-28 18:54:10 +0200

    Render map

--- playground/index.ts
+++ playground/index.ts
@@ -1,36 +1,80 @@
-import { at, Compass, createHexPrototype, Grid, Hex, inStore, move, rectangle } from '../dist'
+import { TILES } from './tiles'
 import { render } from './render'
-import { TILE_ONE } from './tiles'
+import { createHexPrototype, Grid, Hex, rectangle } from '../src'
 
 interface CustomHex extends Hex {
   terrain: string
 }
 
 const hexPrototype = createHexPrototype<CustomHex>({
-  dimensions: { width: 50, height: 50 },
+  dimensions: { width: 60, height: 51.96 },
   orientation: 'flat',
   origin: 'topLeft',
 })
-// const hex = createHex(hexPrototype, { q: 4, r: 3 })
 
 const grid = new Grid(hexPrototype, rectangle({ width: 12, height: 9 }))
-  .traverse([at({ q: 0, r: 0 }), move(Compass.SE), move(Compass.NE)])
-  // .traverse([start({ q: 9, r: 0 }), line(Compass.SE, 4), line(Compass.SW, 4)])
-  .filter(inStore)
-  .each(render)
+// .traverse([at({ q: 0, r: 0 }), move(Compass.SE), move(Compass.NE)])
+// .filter(inStore)
+// .each(render)
+// .run()
+// console.log(grid.store)
 
-// TILE_ONE.forEach((terrain, index) => {
-for (let i = 0; i < 3; i++) {
-  const terrain = TILE_ONE[i]
-  const index = i
-  const hex = grid.getHex({ q: index, r: 0 })
-  hex.terrain = terrain
-  grid.store.set(`${index},${0}`, hex)
-  console.log(`grid[${index}][${0}] = ${terrain}`)
+const tilesToArray = (tilesOrder: string[]) => {
+  const tilesIndex: number[] = []
+
+  const tiles: string[][] = []
+  tilesOrder.forEach((tile) => {
+    const tileIndex = Number(tile[0]) - 1 // convert to zero-based index
+    tilesIndex.push(tileIndex)
+
+    const isFlipped = !!tile[1]
+    tiles.push(isFlipped ? TILES[tileIndex].reverse() : TILES[tileIndex])
+  })
+
+  const result = []
+
+  for (let pair = 0; pair < 3; pair++) {
+    for (let j = 0; j < 3; j++) {
+      for (let coordinate = 0; coordinate < 6; coordinate++) {
+        const leftTiles = tiles[tilesIndex[pair * 2]]
+        result.push(leftTiles[j * 6 + coordinate])
+      }
+      for (let coordinate = 0; coordinate < 6; coordinate++) {
+        const rightTiles = tiles[tilesIndex[pair * 2 + 1]]
+        result.push(rightTiles[j * 6 + coordinate])
+      }
+    }
+  }
+
+  return result
 }
-// })
 
-grid.traverse([move(Compass.SE)])
+const renderTiles = (tilesOrder: string[]) => {
+  const hexagonsOrdered = []
+  const tilesInArray = tilesToArray(tilesOrder)
 
-grid.run()
-console.log(grid.store)
+  // grid.each(render)
+  let index = 0
+  grid
+    .each((hex, grid) => {
+      hex.terrain = tilesInArray[index]
+      hexagonsOrdered.push(hex)
+      // grid.store.set(`${hex.q},${hex.r}`, hex)
+      // console.log('tilesInArray[' + index + ']:', tilesInArray[index])
+      index++
+      render(hex)
+    })
+    .run()
+}
+
+document.querySelectorAll('.js-tilesPosition').forEach((el) => {
+  el.addEventListener('input', () => {
+    const values = [...document.querySelectorAll('.js-tilesPosition')].map((el) => el.value)
+    document.querySelectorAll('.js-tilesPositionCheckbox').forEach((el, i) => {
+      values[i] += el.checked ? 'r' : ''
+    })
+    renderTiles(values)
+  })
+})
+
+renderTiles(['1', '2', '3', '4', '5', '6'])
--- playground/render.ts
+++ playground/render.ts
@@ -3,26 +3,57 @@ import { D, F, M, S, W } from './tiles'
 
 declare const SVG: any
 
-const draw = SVG().addTo('body').size('100%', '100%')
+const draw = SVG().addTo('.js-map').size('100%', '100%')
 
-export const render = (hex: Hex & { terrain?: string }): void => {
+const fillHexagon = (hex: HexWithTerrain) => {
   let fill = 'none'
   if (hex.terrain?.includes(F)) {
-    fill = 'green'
+    fill = '#009900'
   } else if (hex.terrain?.includes(W)) {
-    fill = '#426eff'
+    fill = '#2596be'
   } else if (hex.terrain?.includes(D)) {
-    fill = 'yellow'
+    fill = '#ffcc00'
   } else if (hex.terrain?.includes(M)) {
     fill = 'gray'
   } else if (hex.terrain?.includes(S)) {
     fill = 'purple'
   }
+  const polygon = draw.polygon(hex.corners.map(({ x, y }) => `${x},${y}`)).fill(fill)
+  draw.group().add(polygon)
+}
 
-  const polygon = draw
-    .polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
-    .fill(fill)
-    .stroke({ width: 1, color: '#999' })
+type HexWithTerrain = Hex & { terrain?: string }
+
+const BORDER_DISTANCE = 3
+const DX = [-0.75, -1, -0.75, 0.75, 1, 0.75]
+const DY = [0.75, 0, -0.75, -0.75, 0, 0.75]
+const addBearsAndCougars = (hex: HexWithTerrain) => {
+  if (!hex.terrain?.includes('bears') && !hex.terrain?.includes('cougars')) {
+    return
+  }
+
+  const stroke: any = { width: 1.5 }
+  if (hex.terrain?.includes('bears')) {
+    stroke.color = '#000'
+    stroke.dasharray = 4
+  } else if (hex.terrain?.includes('cougars')) {
+    stroke.color = '#c00'
+  }
+
+  const polygon2 = draw
+    .polygon(
+      hex.corners.map(({ x, y }, i) => {
+        x += BORDER_DISTANCE * DX[i]
+        y += BORDER_DISTANCE * DY[i]
+        return `${x},${y}`
+      }),
+    )
+    .fill('none')
+    .stroke(stroke)
+  draw.group().add(polygon2)
+}
+
+const addCoordinates = (hex: Hex) => {
   const text = draw
     .text(`${hex.q},${hex.r}`)
     // .text(`${hex.col},${hex.row}`)
@@ -33,9 +64,15 @@ export const render = (hex: Hex & { terrain?: string }): void => {
       leading: 0,
     })
     .translate(hex.x, hex.y)
-  console.log({ custom: hex.terrain })
-  draw.group().add(polygon)
   draw.add(text)
+}
+
+export const render = (hex: HexWithTerrain): void => {
+  fillHexagon(hex)
+
+  addBearsAndCougars(hex)
+
+  addCoordinates(hex)
 
   return draw
 }
